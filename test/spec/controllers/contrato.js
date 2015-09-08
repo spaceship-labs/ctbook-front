5965669197f151c0e418639a0233c3a0015ec88c
el-sonny <sonny@spaceshiplabs.com>
2015-09-07 20:56:47 -0500

    fix test

--- test/spec/controllers/contrato.js
+++ test/spec/controllers/contrato.js
@@ -9,8 +9,9 @@ describe('Controller: ContratoCtrl', function () {
     scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, ctbookApi) {
     scope = $rootScope.$new();
+    scope.ctbookApi = ctbookApi;
     ContratoCtrl = $controller('ContratoCtrl', {
       $scope: scope
       // place here mocked dependencies
