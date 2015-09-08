4c0799b7ca13d0f6ab9c8859d8fe9c1e24578b9e
Luis <luis19prz@gmail.com>
2015-09-08 10:53:01 -0500

    responsive layout

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,7 @@ angular.module('ctbookApp')
   .controller('MainCtrl', function($scope, ctbookApi, ctbookRoutes) {
     $scope.ctbookApi = ctbookApi;
     $scope.ctbookRoutes = ctbookRoutes;
+    $scope.toggleFilters = true;
 
     $scope.numberFormat = function(number) {
       if (number === undefined || isNaN(number)) {
@@ -20,4 +21,14 @@ angular.module('ctbookApp')
       number = number.toFixed(2);
       return new Intl.NumberFormat().format(number);
     };
+
+    $scope.contratosActionMenu = function(){
+      if($scope.ctbookRoutes.basePath() != "contratos"){
+        $scope.ctbookRoutes.path = "/contratos";
+      }else{
+        $scope.toggleFilters = !$scope.toggleFilters;
+        console.log('toggle');
+        console.log($scope.toggleFilters);
+      }
+    }
   });
